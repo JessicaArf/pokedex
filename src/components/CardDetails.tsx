b3dc82f691e8de0e67f02e401f8dd6facfee6e46
Jessica Arf <jessicalealarf8@gmail.com>
2022-10-16 14:48:26 -0300

    Function of listing pokemons and LoadingPokebola component created

--- src/components/CardDetails.tsx
+++ src/components/CardDetails.tsx
@@ -8,8 +8,8 @@ type pokemonTypeProps = {
   }
 }
 
-type CardProps = {
-  pokemonNumber: number;
+export type CardProps = {
+  id: number;
   name: string;
   types: Array<pokemonTypeProps>
 
@@ -17,10 +17,10 @@ type CardProps = {
 
 export const CardDetails = (props: CardProps) => {
   return (
-<Link to={`/details/${props.pokemonNumber}`}>
+<Link to={`/details/${props.id}`}>
 <div className= {`card type--${props.types[0].type.name.toLocaleLowerCase()}`}>
 <div className="info">
-<span className="info__number">#{props.pokemonNumber.toString().padStart(3, "0")}</span>
+<span className="info__number">#{props.id.toString().padStart(3, "0")}</span>
 <p className="info__name">{props.name}</p>
 {props.types.map((item, index) => {
   return <Badge key={index} name={item.type.name}/>
@@ -28,7 +28,7 @@ export const CardDetails = (props: CardProps) => {
 <div className="infoType"></div>
 </div>
 <img className="card__img" 
-src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${props.pokemonNumber}.png`}
+src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${props.id}.png`}
 alt={props.name} />
 </div>
 </Link>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,52 @@
-import { useEffect } from "react"
-import {CardDetails} from "../components/CardDetails"
+import { useEffect, useState } from "react"
+import {CardDetails, CardProps} from "../components/CardDetails"
 import {NavBar} from "../components/NavBar"
-import {pokemons} from "../assets/pokemon.json"
 import { api } from "../services/api"
+import { LoadingPokebola } from "../components/LoadingPokebola"
 import './Home.css'
 
 export const Home = () => {
 
+  const [ isLoading, setIsLoading] = useState(true);
+  const [pokemonList, setPokemonList] = useState <CardProps[]>([]);;
+
   async function getPokemonData() {
-  const {data} = await api.get("pokemon?limit=151&offset=0")
-  console.log("dados:",data.results)}
+  const {data} = await api.get("pokemon?limit=151")
+
+  const dataComplete = await Promise.all(
+    data.results.map(async ( result: {url: string}) => {
+      const { data }= await api.get(result.url);
+
+      return {
+        id: data.id,
+        name: data.name,
+        types: data.types,
+      };
+    })
+  )
+  setPokemonList(dataComplete);
+  setIsLoading(false)
+  };
+
 
   useEffect(() => {
   getPokemonData();
-  }, [])
+  }, []);
 
 
-
+  if(isLoading){
+    return <LoadingPokebola message="Carregando.."/>
+  }
+  
   return (
     <> 
     <NavBar/>
     <h1 className="title">Encontre todos os pokémons em um só lugar</h1>
     <div className="list">
-    {pokemons.map((pokemon, index)=> (
+    {pokemonList.map((pokemon, index)=> (
     <CardDetails  
     key={index}
-    pokemonNumber={pokemon.id}
+    id={pokemon.id}
     name={pokemon.name}
     types={pokemon.types}
     />))}
