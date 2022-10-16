7daca095d7452ecf77798a23d89f5add1360cd36
Jessica Arf <jessicalealarf8@gmail.com>
2022-10-15 23:43:15 -0300

    connection created with pokemon api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react"
 import {CardDetails} from "../components/CardDetails"
 import {NavBar} from "../components/NavBar"
-import pokemons from "../assets/pokemon.json"
+import {pokemons} from "../assets/pokemon.json"
+import { api } from "../services/api"
 import './Home.css'
 
 export const Home = () => {
+
+  async function getPokemonData() {
+  const {data} = await api.get("pokemon?limit=151&offset=0")
+  console.log("dados:",data.results)}
+
+  useEffect(() => {
+  getPokemonData();
+  }, [])
+
+
+
   return (
     <> 
     <NavBar/>
